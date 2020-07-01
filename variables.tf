26c8c90e4ffccc7370fcb69f2612b9464ec7bdf3
Matias Manavella <matiasm@nimble.la>
2020-07-01 19:08:03 -0300

    variables redefined

--- variables.tf
+++ variables.tf
@@ -1,7 +1,12 @@
 variable "region" {
   type = string
+  default = "us-east-1"
 }
 
 variable "amis" {
   type = map(string)
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-4b32be2b"
+  }
 }
