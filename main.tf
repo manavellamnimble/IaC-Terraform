b2e84119daadde47e70299e63c53497e0f89845c
Matias Manavella <matiasm@nimble.la>
2020-07-01 18:59:58 -0300

    Formatted

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   version = "~> 2.68"
-  region = var.region
+  region  = var.region
   profile = "default"
 }
 # Adds EC2 instance
 resource "aws_instance" "example" {
-  ami = var.amis[var.region]
+  ami           = var.amis[var.region]
   instance_type = "t2.micro"
 }
 
